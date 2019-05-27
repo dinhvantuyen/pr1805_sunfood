3c1a9efa2608d5fbcc01f442d923475ea4885ef9
Luan Nguyen <thanhluanstda@gmail.com>
2019-05-27 20:02:13 +0700

    divise

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_16_071044) do
+ActiveRecord::Schema.define(version: 2019_05_27_121148) do
 
   create_table "addresses", force: :cascade do |t|
     t.integer "phone"
@@ -111,11 +111,17 @@ ActiveRecord::Schema.define(version: 2019_05_16_071044) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
-    t.string "email"
     t.string "password"
     t.string "type"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
