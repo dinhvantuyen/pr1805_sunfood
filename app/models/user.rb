ed5ef3cefea996901696dfe062b3109b4cabf64e
Luan Nguyen <thanhluanstda@gmail.com>
2019-06-04 14:40:31 +0700

    product-show

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ApplicationRecord
   has_many :messages
   has_many :conversations, foreign_key: :sender_id
   has_many :orders
-  has_many :reviews
+  has_many :reviews, dependent: :destroy
   has_many :comments
   has_many :addresses
 
--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,7 @@ Rails.application.routes.draw do
     namespace :admin do
       root "dashboard#index"
     end
+
+    resources :reviews
   end
 end
